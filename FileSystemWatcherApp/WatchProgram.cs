981d976bf528e4c6868d255c3791abf5d15c5517
DavidGu <35072445+ChengWeiGu@users.noreply.github.com>
2021-11-10 23:30:09 +0800

    update on 11/10

---
+++ FileSystemWatcherApp/WatchProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FileSystemWatcherConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var monitoringPath = @"D:\TestFileWatcher\";
+            Console.WriteLine("Monitoring file changed in target folder.");
+            Console.WriteLine($"Target Folder: {monitoringPath}");
+
+            new Watcher(monitoringPath)
+                .WatchCreated()
+                .WatchChanged();
+
+            Console.ReadLine();
+        }
+    }
+}
